696fdcfe018ed7ce747c68998784dd89eccfbe6a
oskarskalski <os.sk.gr@gmail.com>
2021-07-24 20:54:25 +0200

    fixed tests

--- src/test/java/test/pl/oskarskalski/TestData.java
+++ src/test/java/test/pl/oskarskalski/TestData.java
@@ -14,30 +14,34 @@ class TestData {
     static final String VEHICLE_BRAND_NAME = "Kross";
     static final boolean VEHICLE_IS_ELECTRICAL = false;
     static final int VEHICLE_NUMBER_OF_SEATS = 1;
-    static final int VEHICLE_NUMBER_OF_WHEELS = 2;
+    static final int VEHICLE_NUMBER_OF_TIRES = 2;
     static final String VEHICLE_TYPE = "MTB";
 
     static final String BIKE_BRAND_NAME = "Trek";
     static final double BIKE_PRICE = 4000.0;
     static final boolean BIKE_IS_ELECTRICAL = true;
+    static final int BIKE_NUMBER_OF_SEATS = VEHICLE_NUMBER_OF_SEATS;
+    static final int BIKE_NUMBER_OF_TIRES = VEHICLE_NUMBER_OF_TIRES;
+    static final String BIKE_TYPE = VEHICLE_TYPE;
 
     static final String MOTORCYCLE_BRAND_NAME = "Yamaha";
     static final String MOTORCYCLE_ENGINE_TYPE = "inline_4";
     static final int MOTORCYCLE_HORSE_POWER = 150;
-    static final double MOTOrCYCLE_MILEAGE = 1.0;
+    static final double MOTORCYCLE_MILEAGE = 1.0;
     static final int MOTORCYCLE_NUMBER_OF_SEATS = 2;
-    static final int MOTORCYCLE_NUMBER_OF_WHEELS = 2;
+    static final int MOTORCYCLE_NUMBER_OF_TIRES = 2;
     static final double MOTORCYCLE_PRICE = 15000.0;
     static final String MOTORCYCLE_TRANSMISSION_TYPE = "manual";
     static final String MOTORCYCLE_TYPE = "street";
+    static final boolean MOTORCYCLE_IS_ELECTRICAL = false;
 
     static final String CAR_BRAND_NAME = "Tesla";
-    static final String CAR_ENGINE_TYPE = null;
+    static final String CAR_ENGINE_TYPE = "3-phase_AC";
     static final boolean CAR_IS_ELECTRICAL = true;
     static final int CAR_HORSE_POWER = 400;
     static final double CAR_MILEAGE = 1.0;
     static final int CAR_NUMBER_OF_SEATS = 5;
-    static final int CAR_NUMBER_OF_WHEELS = 4;
+    static final int CAR_NUMBER_OF_TIRES = 4;
     static final double CAR_PRICE = 300_000.0;
     static final String CAR_TRANSMISSION_TYPE = "automatic";
     static final String CAR_TYPE = "suv";
@@ -45,19 +49,19 @@ class TestData {
     static final boolean CAR_HAS_ROOF_BIKE_RACK = false;
 
     static final String VEHICLE_TEXT_DATA = VEHICLE_PREFIX + VEHICLE_BRAND_NAME + " " + VEHICLE_IS_ELECTRICAL + " " +
-            VEHICLE_NUMBER_OF_SEATS + " " + VEHICLE_NUMBER_OF_WHEELS + " " + VEHICLE_TYPE;
+            VEHICLE_NUMBER_OF_SEATS + " " + VEHICLE_NUMBER_OF_TIRES + " " + VEHICLE_TYPE;
 
     static final String BIKE_TEXT_DATA = BIKE_PREFIX + BIKE_BRAND_NAME + " " + BIKE_IS_ELECTRICAL + " " +
-            VEHICLE_NUMBER_OF_SEATS + " " + VEHICLE_NUMBER_OF_WHEELS + " " + BIKE_PRICE + " " + VEHICLE_TYPE;
+            BIKE_NUMBER_OF_SEATS + " " + BIKE_NUMBER_OF_TIRES + " " + BIKE_PRICE + " " + BIKE_TYPE;
 
 
-    static final String MOTORCYCLE_TEXT_DATA = MOTORCYCLE_PREFIX + MOTORCYCLE_BRAND_NAME + " " + VEHICLE_IS_ELECTRICAL +
-            " " + MOTORCYCLE_ENGINE_TYPE + " " + +MOTORCYCLE_HORSE_POWER + " " + MOTOrCYCLE_MILEAGE + " " + MOTORCYCLE_NUMBER_OF_SEATS + " " +
-            MOTORCYCLE_NUMBER_OF_WHEELS + " " + MOTORCYCLE_PRICE + " " + MOTORCYCLE_TRANSMISSION_TYPE + " " + MOTORCYCLE_TYPE;
+    static final String MOTORCYCLE_TEXT_DATA = MOTORCYCLE_PREFIX + MOTORCYCLE_BRAND_NAME + " " + MOTORCYCLE_IS_ELECTRICAL +
+            " " + MOTORCYCLE_ENGINE_TYPE + " " + +MOTORCYCLE_HORSE_POWER + " " + MOTORCYCLE_MILEAGE + " " + MOTORCYCLE_NUMBER_OF_SEATS + " " +
+            MOTORCYCLE_NUMBER_OF_TIRES + " " + MOTORCYCLE_PRICE + " " + MOTORCYCLE_TRANSMISSION_TYPE + " " + MOTORCYCLE_TYPE;
 
-    static final String CAR_TEXT_DATA = CAR_PREFIX + CAR_BRAND_NAME + " " + CAR_IS_ELECTRICAL +" "+ CAR_ENGINE_TYPE + " " +
-            CAR_HORSE_POWER + " " + CAR_MILEAGE + " " + CAR_NUMBER_OF_SEATS + " " + CAR_NUMBER_OF_WHEELS + " " +
-            CAR_PRICE + " " +  CAR_HAS_TOW_BAR + " " + CAR_HAS_ROOF_BIKE_RACK + " " +  CAR_TRANSMISSION_TYPE + " " + CAR_TYPE;
+    static final String CAR_TEXT_DATA = CAR_PREFIX + CAR_BRAND_NAME + " " + CAR_IS_ELECTRICAL + " " + CAR_ENGINE_TYPE + " " +
+            CAR_HORSE_POWER + " " + CAR_MILEAGE + " " + CAR_NUMBER_OF_SEATS + " " + CAR_NUMBER_OF_TIRES + " " +
+            CAR_PRICE + " " + CAR_HAS_TOW_BAR + " " + CAR_HAS_ROOF_BIKE_RACK + " " + CAR_TRANSMISSION_TYPE + " " + CAR_TYPE;
 
 
     static final Car CAR_OBJECT_TEST_WITH_NULL_FIELDS = new Car();
--- src/test/java/test/pl/oskarskalski/VehicleMapperTests.java
+++ src/test/java/test/pl/oskarskalski/VehicleMapperTests.java
@@ -20,11 +20,11 @@ class VehicleMapperTests {
         String[] data = VEHICLE_TEXT_DATA.split(" ");
         Vehicle vehicle = vehicleMapper.mapToObject(new Vehicle(), data);
 
-        assertAll(() -> assertEquals(vehicle.getBrandName(), "Kross"),
-                () -> assertEquals(vehicle.getNumberOfSeats(), 1),
-                () -> assertEquals(vehicle.getNumberOfTires(), 2),
-                ()-> assertEquals(vehicle.getVehicleType(), "MTB"),
-                () -> assertFalse(vehicle.isElectrical()));
+        assertAll(() -> assertEquals(VEHICLE_BRAND_NAME, vehicle.getBrandName()),
+                () -> assertEquals(VEHICLE_NUMBER_OF_SEATS, vehicle.getNumberOfSeats()),
+                () -> assertEquals(VEHICLE_NUMBER_OF_TIRES, vehicle.getNumberOfTires()),
+                () -> assertEquals(VEHICLE_TYPE, vehicle.getVehicleType()),
+                () -> assertEquals(VEHICLE_IS_ELECTRICAL, vehicle.isElectrical()));
     }
 
     @Test
@@ -33,12 +33,12 @@ class VehicleMapperTests {
         String[] data = BIKE_TEXT_DATA.split(" ");
         Bike vehicle = vehicleMapper.mapToObject(new Bike(), data);
 
-        assertAll(() -> assertEquals(vehicle.getBrandName(), "Trek"),
-                () -> assertEquals(vehicle.getNumberOfSeats(), 1),
-                () -> assertEquals(vehicle.getNumberOfTires(), 2),
-                ()-> assertEquals(vehicle.getVehicleType(), "MTB"),
-                ()-> assertEquals(vehicle.getPrice(), 4000.0),
-                () -> assertTrue(vehicle.isElectrical()));
+        assertAll(() -> assertEquals(BIKE_BRAND_NAME, vehicle.getBrandName()),
+                () -> assertEquals(BIKE_NUMBER_OF_SEATS, vehicle.getNumberOfSeats()),
+                () -> assertEquals(BIKE_NUMBER_OF_TIRES, vehicle.getNumberOfTires()),
+                () -> assertEquals(BIKE_TYPE, vehicle.getVehicleType()),
+                () -> assertEquals(BIKE_PRICE, vehicle.getPrice()),
+                () -> assertEquals(BIKE_PRICE, vehicle.getPrice()));
     }
 
     @Test
@@ -47,16 +47,16 @@ class VehicleMapperTests {
         String[] data = MOTORCYCLE_TEXT_DATA.split(" ");
         Motorcycle vehicle = vehicleMapper.mapToObject(new Motorcycle(), data);
 
-        assertAll(() -> assertEquals(vehicle.getBrandName(), "Yamaha"),
-                () -> assertEquals(vehicle.getNumberOfSeats(), 2),
-                () -> assertEquals(vehicle.getNumberOfTires(), 2),
-                ()-> assertEquals(vehicle.getVehicleType(), "street"),
-                ()-> assertEquals(vehicle.getPrice(), 15000.0),
-                ()-> assertEquals(vehicle.getTransmissionsType(), "manual"),
-                ()-> assertEquals(vehicle.getEngineType(), "inline_4"),
-                ()-> assertEquals(vehicle.getHorsePower(), 150),
-                ()-> assertEquals(vehicle.getMileage(), 1.0),
-                () -> assertFalse(vehicle.isElectrical()));
+        assertAll(() -> assertEquals(MOTORCYCLE_BRAND_NAME, vehicle.getBrandName()),
+                () -> assertEquals(MOTORCYCLE_NUMBER_OF_SEATS, vehicle.getNumberOfSeats()),
+                () -> assertEquals(MOTORCYCLE_NUMBER_OF_TIRES, vehicle.getNumberOfTires()),
+                () -> assertEquals(MOTORCYCLE_TYPE, vehicle.getVehicleType()),
+                () -> assertEquals(MOTORCYCLE_PRICE, vehicle.getPrice()),
+                () -> assertEquals(MOTORCYCLE_TRANSMISSION_TYPE, vehicle.getTransmissionsType()),
+                () -> assertEquals(MOTORCYCLE_ENGINE_TYPE, vehicle.getEngineType()),
+                () -> assertEquals(MOTORCYCLE_HORSE_POWER, vehicle.getHorsePower()),
+                () -> assertEquals(MOTORCYCLE_MILEAGE, vehicle.getMileage()),
+                () -> assertEquals(MOTORCYCLE_IS_ELECTRICAL, vehicle.isElectrical()));
     }
 
     @Test
@@ -65,18 +65,18 @@ class VehicleMapperTests {
         String[] data = CAR_TEXT_DATA.split(" ");
         Car vehicle = vehicleMapper.mapToObject(new Car(), data);
 
-        assertAll(() -> assertEquals(vehicle.getBrandName(), "Tesla"),
-                () -> assertEquals(vehicle.getNumberOfSeats(), 5),
-                () -> assertEquals(vehicle.getNumberOfTires(), 4),
-                ()-> assertEquals(vehicle.getVehicleType(), "suv"),
-                ()-> assertEquals(vehicle.getPrice(), 300000.0),
-                ()-> assertEquals(vehicle.getTransmissionsType(), "automatic"),
-                ()-> assertEquals(vehicle.getEngineType(), "null"),
-                ()-> assertFalse(vehicle.hasRoofBikeRack()),
-                ()-> assertFalse(vehicle.hasTowBar()),
-                ()-> assertEquals(vehicle.getHorsePower(), 400),
-                ()-> assertEquals(vehicle.getMileage(), 1.0),
-                () -> assertTrue(vehicle.isElectrical()));
+        assertAll(() -> assertEquals(CAR_BRAND_NAME, vehicle.getBrandName()),
+                () -> assertEquals(CAR_NUMBER_OF_SEATS, vehicle.getNumberOfSeats()),
+                () -> assertEquals(CAR_NUMBER_OF_TIRES, vehicle.getNumberOfTires()),
+                () -> assertEquals(CAR_TYPE, vehicle.getVehicleType()),
+                () -> assertEquals(CAR_PRICE, vehicle.getPrice(), 300000.0),
+                () -> assertEquals(CAR_TRANSMISSION_TYPE, vehicle.getTransmissionsType()),
+                () -> assertEquals(CAR_ENGINE_TYPE, vehicle.getEngineType()),
+                () -> assertEquals(CAR_HAS_ROOF_BIKE_RACK, vehicle.hasRoofBikeRack()),
+                () -> assertEquals(CAR_HAS_TOW_BAR, vehicle.hasTowBar()),
+                () -> assertEquals(CAR_HORSE_POWER, vehicle.getHorsePower()),
+                () -> assertEquals(CAR_MILEAGE, vehicle.getMileage()),
+                () -> assertEquals(CAR_IS_ELECTRICAL, vehicle.isElectrical()));
     }
 
 
@@ -86,10 +86,10 @@ class VehicleMapperTests {
         String[] data = VEHICLE_TEXT_DATA.substring(0, VEHICLE_TEXT_DATA.length() - 4).split(" ");
         Vehicle vehicle = vehicleMapper.mapToObject(new Vehicle(), data);
 
-        assertAll(() -> assertEquals(vehicle.getBrandName(), "Kross"),
-                () -> assertEquals(vehicle.getNumberOfSeats(), 1),
-                () -> assertEquals(vehicle.getNumberOfTires(), 2),
-                () -> assertNull(vehicle.getVehicleType()),
-                () -> assertFalse(vehicle.isElectrical()));
+        assertAll(() -> assertEquals(VEHICLE_BRAND_NAME, vehicle.getBrandName()),
+                () -> assertEquals(VEHICLE_NUMBER_OF_SEATS, vehicle.getNumberOfSeats()),
+                () -> assertEquals(VEHICLE_NUMBER_OF_TIRES, vehicle.getNumberOfTires()),
+                () -> assertEquals(VEHICLE_IS_ELECTRICAL, vehicle.isElectrical()),
+                () -> assertNull(vehicle.getVehicleType()));
     }
 }
